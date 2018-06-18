e02857cf3b105838accaac0cd0164311a2b4dd2d
Gabriela Vitorino <gabrielabvitorino@gmail.com>
2018-06-18 11:00:31 -0300

    Pulo

--- Assets/Scripts/PlayCtrl.cs
+++ Assets/Scripts/PlayCtrl.cs
@@ -6,6 +6,8 @@ public class PlayCtrl : MonoBehaviour {
 
     public float horizontalSpeed = 10f;
 
+    public float jumpSpeed = 600f;
+
     Rigidbody2D rb;
 
 	// Use this for initialization
@@ -26,6 +28,10 @@ public class PlayCtrl : MonoBehaviour {
             StopMovingHorizontal();
         }
 
+        if (Input.GetButtonDown("Jump")) {
+            Jump();
+        }
+
 	}
     
     void MoveHorizontal(float speed) {
@@ -34,5 +40,9 @@ public class PlayCtrl : MonoBehaviour {
 
     void StopMovingHorizontal() {
         rb.velocity = new Vector2(0f, rb.velocity.y);
+    }
+
+    void Jump() {
+        rb.AddForce(new Vector2(0f, jumpSpeed));
     }
 }
