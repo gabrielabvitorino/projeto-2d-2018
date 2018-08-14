c1aaa1e4c910ebe976b64b0168e2c454853bd558
Gabriela Vitorino <gabrielabvitorino@gmail.com>
2018-08-14 10:40:41 -0300

    Preparando GameOver

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -23,7 +23,23 @@ public class UI {
         [Header("Other")]
         public GameObject HudPanel;
     }
+    
 
-   
+    [Serializable]
+
+    public class GameOver
+    {
+        [Header("Text")]
+        public Text txtCoinCount;
+        
+        public Text txtTimer;
+
+
+        [Header("Other")]
+        public GameObject GameOverPanel;
+    }
+
+    public GameOver gameOver;
+
     public HUD hud;
 }
